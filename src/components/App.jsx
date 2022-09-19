50bdf3eb3c7fd55a12cde8b982df3413dad177bc
Swarna Sarker <swarnaprony81@gmail.com>
2022-09-19 19:41:24 +0200

    Add id in cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,25 @@ import Card from "./Card";
 import contacts from "./contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return <Card 
+  id={contact.id}
+  key={contact.id}
+  name={contact.name}
+  img={contact.imgURL}
+  tel={contact.phone}
+  email={contact.email}
+  />
+}
+
 
 function App() {
   return (<div>
     <h1 className="heading">My Contacts</h1>
     <Avatar img="https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?cs=srgb&dl=pexels-pixabay-56866.jpg&fm=jpg" />
   
-    <Card 
+    {contacts.map(createCard)}
+    {/* <Card 
       name={contacts[0].name}
       img={contacts[0].imgURL}
       tel={contacts[0].tel}
@@ -28,7 +40,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].tel}
         email = {contacts[2].email}     
-        />
+        /> */}
   </div>)
 }
 
